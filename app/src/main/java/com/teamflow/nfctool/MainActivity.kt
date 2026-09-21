9ac5ab1d982e0cea4e8f04c02be3d18ee05f6a8b
Arish <webdeveloper2@amvionlabs.com>
2026-09-21 15:42:07 +0530

    compileDebugKotlin are resolved

--- app/src/main/java/com/teamflow/nfctool/MainActivity.kt
+++ app/src/main/java/com/teamflow/nfctool/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
